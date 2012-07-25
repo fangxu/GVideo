1de58b2bee13dc01035f90f4b381bcf683194ad8
fang <fangux@gmail.com>
2012-07-25 09:44:40 +0800

    first commit

---
+++ Video for G1/FormMain.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace Video_for_G1
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView = new System.Windows.Forms.ListView();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.buttonRemove = new System.Windows.Forms.Button();
+            this.textBoxOutput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonOpen = new System.Windows.Forms.Button();
+            this.textBoxOptions = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listView
+            // 
+            this.listView.GridLines = true;
+            this.listView.Location = new System.Drawing.Point(12, 3);
+            this.listView.Name = "listView";
+            this.listView.Size = new System.Drawing.Size(493, 213);
+            this.listView.TabIndex = 0;
+            this.listView.UseCompatibleStateImageBehavior = false;
+            this.listView.View = System.Windows.Forms.View.Details;
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(13, 223);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(75, 23);
+            this.buttonStart.TabIndex = 1;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Location = new System.Drawing.Point(113, 223);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(75, 23);
+            this.buttonClear.TabIndex = 2;
+            this.buttonClear.Text = "Clear";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // buttonRemove
+            // 
+            this.buttonRemove.Location = new System.Drawing.Point(213, 223);
+            this.buttonRemove.Name = "buttonRemove";
+            this.buttonRemove.Size = new System.Drawing.Size(75, 23);
+            this.buttonRemove.TabIndex = 3;
+            this.buttonRemove.Text = "Remove";
+            this.buttonRemove.UseVisualStyleBackColor = true;
+            this.buttonRemove.Click += new System.EventHandler(this.buttonRemove_Click);
+            // 
+            // textBoxOutput
+            // 
+            this.textBoxOutput.Location = new System.Drawing.Point(65, 251);
+            this.textBoxOutput.Name = "textBoxOutput";
+            this.textBoxOutput.Size = new System.Drawing.Size(352, 21);
+            this.textBoxOutput.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 255);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 12);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Output:";
+            // 
+            // buttonOpen
+            // 
+            this.buttonOpen.Location = new System.Drawing.Point(430, 249);
+            this.buttonOpen.Name = "buttonOpen";
+            this.buttonOpen.Size = new System.Drawing.Size(75, 23);
+            this.buttonOpen.TabIndex = 6;
+            this.buttonOpen.Text = "Open";
+            this.buttonOpen.UseVisualStyleBackColor = true;
+            this.buttonOpen.Click += new System.EventHandler(this.buttonOpen_Click);
+            // 
+            // textBoxOptions
+            // 
+            this.textBoxOptions.Location = new System.Drawing.Point(65, 278);
+            this.textBoxOptions.Name = "textBoxOptions";
+            this.textBoxOptions.Size = new System.Drawing.Size(440, 21);
+            this.textBoxOptions.TabIndex = 7;
+            this.textBoxOptions.Text = "--profile baseline --level 3 --tune animation --crf 23 --vbv-bufsize 2500 --vbv-m" +
+                "axrate 2500 --vf resize:480,320,,both,,spline";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 281);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Options:";
+            // 
+            // FormMain
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(517, 307);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxOptions);
+            this.Controls.Add(this.buttonOpen);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxOutput);
+            this.Controls.Add(this.buttonRemove);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.listView);
+            this.Name = "FormMain";
+            this.Text = "Video for G1";
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.FormMain_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.FormMain_DragEnter);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.Button buttonRemove;
+        private System.Windows.Forms.TextBox textBoxOutput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonOpen;
+        private System.Windows.Forms.TextBox textBoxOptions;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Video for G1/FormMain.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using System.Threading;
+using System.IO;
+using System.Diagnostics;
+
+namespace Video_for_G1
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+            listView.Columns.Add("file", -2, HorizontalAlignment.Left);
+            listView.Columns.Add("statue", -2, HorizontalAlignment.Left);
+        }
+
+        private void FormMain_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.Copy;
+        }
+
+        private void FormMain_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] stringTemp = (string[])e.Data.GetData(DataFormats.FileDrop);           
+            textBoxOutput.Text = stringTemp[0].Substring(0, stringTemp[0].LastIndexOf('\\'));
+            for (int i = 0; i < stringTemp.Length; i++)
+            {
+                ListViewItem it = new ListViewItem();
+                it.Text = stringTemp[i];
+                it.SubItems.Add("waiting");
+                listView.Items.Add(it);
+            }
+            listView.Columns[0].Width = -1;
+            listView.Columns[1].Width = -1;
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            listView.Clear();
+            listView.Columns.Add("file", -2, HorizontalAlignment.Left);
+            listView.Columns.Add("statue", -2, HorizontalAlignment.Left);
+        }
+
+        private void buttonOpen_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dlg = new FolderBrowserDialog();
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                textBoxOutput.Text = dlg.SelectedPath;
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        private void buttonRemove_Click(object sender, EventArgs e)
+        {
+            int n = listView.SelectedItems.Count;
+            for (int i = 0; i < n; i++)
+            {
+                listView.Items.Remove(listView.SelectedItems[0]);
+            }
+
+        }
+
+        private void Output(object sendProcess, DataReceivedEventArgs output)
+        {
+            if (String.IsNullOrEmpty(output.Data)) return;
+            changeTitle(output.Data);
+        }
+
+
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            int count = listView.Items.Count;
+            string[] video = new string[count];
+            string[] state = new string[count];
+            string options = textBoxOptions.Text;
+            string output = textBoxOutput.Text;
+            for (int i = 0; i < count; i++)
+            {
+                video[i] = this.listView.Items[i].SubItems[0].Text;
+                state[i] = this.listView.Items[i].SubItems[1].Text;
+            }
+
+            new Thread(() =>
+            {
+                for (int i = 0; i < count; i++)
+                {
+                    if (state[i] == "waiting")
+                    {
+                        changeStatue(i, "processing");
+                        Process p = new Process();
+                        string path = " \"" + output + video[i].Substring(video[i].LastIndexOf('\\'), video[i].LastIndexOf('.') - video[i].LastIndexOf('\\'));
+                        string vo = path + "_v.mp4\" ";
+                        string ao = path + "_a.m4a\" ";
+                        string avo = path + "_enc.mp4\" ";
+                        string vi = " \"" + video[i] + "\" ";
+                        string ph = Environment.CommandLine;
+                        ph = ph.Substring(0, ph.LastIndexOf('\\') + 1);
+                        if (ph[0] == '"')
+                            ph = ph.Substring(1);
+
+                        string strBatPath = ph + "temp.bat";
+                        if (File.Exists(strBatPath))
+                        {
+                            File.Delete(strBatPath);
+                        }
+
+                        StringBuilder sBuilder = new StringBuilder();
+                        sBuilder.AppendLine("x264 " + options + " -o " + vo + vi);
+                        sBuilder.AppendLine("ffmpeg -i " + vi + " -f wav - | neroaacenc -q 0.28 -if - -ignorelength -of " + ao);
+                        sBuilder.AppendLine("ffmpeg -i " + vo + " -i " + ao + " -vcodec copy -acodec copy " + avo);
+                        sBuilder.AppendLine("del " + vo);
+                        sBuilder.AppendLine("del " + ao);
+                        Stream st = new FileStream(strBatPath, FileMode.OpenOrCreate);
+                        using (StreamWriter sw = new StreamWriter(st))
+                        {
+                            sw.Write(sBuilder.ToString());
+                            sw.Close();
+                            st.Dispose();
+                            st.Close();
+                        }
+
+                        p.StartInfo.FileName = "\"" + ph + "temp.bat\"";
+                        p.StartInfo.RedirectStandardError = true;
+                        p.ErrorDataReceived += new DataReceivedEventHandler(Output);
+                        p.StartInfo.UseShellExecute = false;
+                        p.StartInfo.CreateNoWindow = true;
+                        p.Start();
+                        p.BeginErrorReadLine();
+                        p.WaitForExit();
+                        changeStatue(i, "done");
+                        p.Close();
+                        p.Dispose();
+                    }
+                }
+                changeTitle("Video for G1");
+            }).Start();
+
+        }
+        private void changeStatue(int i, string s)
+        {
+            this.Invoke(new MethodInvoker(() =>
+            {
+                listView.Items[i].SubItems[1].Text = s;
+            }));
+        }
+
+        private void changeTitle(string s)
+        {
+            this.BeginInvoke(new MethodInvoker(() =>
+            {
+                this.Text = s;
+            }));
+        }
+    }
+}
